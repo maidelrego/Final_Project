b951be50a75b8156474412a868660f93e106539f
Mx-Jeckal <mx.jeckal@gmail.com>
2020-05-20 21:31:02 -0500

    chart not included in rendered version

--- client/src/components/Chart/Chart.js
+++ client/src/components/Chart/Chart.js
@@ -1,37 +1,87 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState} from "react";
 import Chart from "chart.js";
-import { nationalAverageQuarterData, quarterLabels } from "./data.js"
+import { nationalAverageQuarterData, quarterLabels } from "./data.js";
+import axios from "axios";
 let myLineChart;
 
 
 //--Chart Style Options--//
-Chart.defaults.global.defaultFontFamily = "'PT Sans', sans-serif"
+Chart.defaults.global.defaultFontFamily = "'PT Sans', sans-serif";
 Chart.defaults.global.legend.display = false;
 //--Chart Style Options--//
 
-export default class LineGraph extends Component {
+let labelss = ["May 1st", "May 15th", "June 1st", "June 15th"];
 
-  chartRef = React.createRef();
+export default function LineGraph() {
+  const [chart, setChart] = useState({});
 
-  componentDidMount() {
-    this.buildChart();
-  }
+  useEffect(() => {
+    getData();
+  }, []);
 
-  buildChart = () => {
-    const myChartRef = this.chartRef.current.getContext("2d");
+  const getData = async () => {
+    try {
+      const res = await axios.get(
+        "https://corona.lmao.ninja/v2/historical/pakistan"
+      );
+
+      setChart({
+        labels: [Object.keys(res.data.timeline.cases)],
+        datasets: [
+          {
+            label: "Covid-19",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: "butt",
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: "miter",
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: Object.values(res.data.timeline.cases)
+          }
+        ]
+      });
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
+
+   
+
+  // componentDidMount() {
+  //   this.loadQuotes();
+  //   this.buildChart();
+  // }
+
+  function buildChart() {
+    let chartRef = React.createRef();
+    const myChartRef = chartRef.current.getContext("2d");
     // const { data, average, labels } = this.props;
 
-    if (typeof myLineChart !== "undefined") myLineChart.destroy();
+    if (typeof myLineChart !== "undefined") {
+      myLineChart.destroy();
+    }
 
     myLineChart = new Chart(myChartRef, {
       type: "bar",
       data: {
         //Bring in data
-        labels: quarterLabels, // ['WE May 1st', 'WE May 7th'] -- use moment to get this array
+        labels: labelss, // ['WE May 1st', 'WE May 7th'] -- use moment to get this array
         datasets: [
           {
-            label: "chicken tendies",
-            data: nationalAverageQuarterData, // [5, 7] -- .find -> .then((data)=> data.length)
+            label: "Week Ending",
+            data: this.loadQuotes(), // [5, 7] -- .find -> .then((data)=> data.length)
             fill: true,
             borderColor: "#6610f2"
           }
@@ -39,22 +89,22 @@ export default class LineGraph extends Component {
         options: {
           //Customize chart options
           backgroundColor: [
-            '#36a2eb'
+            "#36a2eb"
           ]
         }
-      }});
+      }
+    });
 
   }
-  render() {
 
-    return (
-      <div>
-        <canvas
-          id="myChart"
-          ref={this.chartRef}
-        />
-      </div>
-    )
-  }
+  return (
+    <div>
+      <canvas
+        id="myChart"
+        ref={this.chartRef}
+      />
+      {/* <div>{this.props}</div> */}
+    </div>
+  );
 }
 
