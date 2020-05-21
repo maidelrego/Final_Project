15c8e0aa0768cbe4499f1e3efd988d16f7eaa71d
saphirah.pociluyko <sjpcp5@gmail.com>
2020-05-20 20:00:56 -0500

    create contact controller and schema for messages recieved from users

---
+++ controllers/contactController.js
@@ -0,0 +1,33 @@
+const db = require("../models");
+
+// Defining methods for the quoteController
+module.exports = {
+  findAll: function (req, res) {
+    db.Contact.find(req.query)
+      .sort({ date: -1 })
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  findById: function (req, res) {
+    db.Contact.findById(req.params.id)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  create: function (req, res) {
+    console.log("look here", req.body);
+    db.Contact.create(req.body)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  update: function (req, res) {
+    db.Contact.findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  delete: function (req, res) {
+    db.Contact.findById({ _id: req.params.id })
+      .then((dbModel) => dbModel.remove())
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+};
--- routes/api/api-routes.js
+++ routes/api/api-routes.js
@@ -2,8 +2,9 @@ const passport = require("passport");
 const router = require("express").Router();
 const isAuthenticated = require("../../config/isAuthenticated");
 const quotesController = require("../../controllers/quoteController.js");
+const contactController = require("../../controllers/contactController.js");
 
-// Matches with "/api/.."
+// each route matches with "/api/.."
 router
   .route("/quotes")
   .get(quotesController.findAll)
@@ -14,6 +15,18 @@ router
   .get(quotesController.findById)
   .delete(quotesController.delete);
 
+// contact routes
+router
+  .route("/contact")
+  .get(contactController.findAll)
+  .post(contactController.create);
+
+router
+  .route("/contact/:id")
+  .get(contactController.findById)
+  .delete(contactController.delete);
+
+// user login/logout routes
 router.get("/admin", isAuthenticated, (req, res) => {
   res.json({ status: "ok" });
 });
