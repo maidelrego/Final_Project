e6a8db91fe1d3fd7fde203cd78fb11e90f319726
saphirah.pociluyko <sjpcp5@gmail.com>
2020-05-22 12:59:28 -0500

    created a user seed

---
+++ scripts/seedUserDB.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+
+// This file empties the Books collection and inserts the books below
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/campbellwooddesigns"
+);
+
+const UserSeed = [
+  {
+    username: "admin",
+    password: "jacki",
+    role: "admin",
+  },
+];
+
+db.User.remove({})
+  .then(() => db.User.collection.insertMany(UserSeed))
+  .then((data) => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
