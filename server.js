94dbb97ac20309b678b96b914464f016d83aec16
saphirah.pociluyko <sjpcp5@gmail.com>
2020-05-22 12:21:39 -0500

    change port to 8080 and access control allow orgin to https://ancient-lowlands-03560.herokuapp.com/

--- server.js
+++ server.js
@@ -1,30 +1,40 @@
 const express = require("express");
-const passport = require("passport")
+const passport = require("passport");
 const mongoose = require("mongoose");
-const morgan = require('morgan');
-const bodyParser = require('body-parser')
-const session = require('express-session');
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const session = require("express-session");
 const app = express();
-const routes = require('./routes/index');
-const PORT = process.env.PORT || 3001;
+const routes = require("./routes/index");
+const PORT = process.env.PORT || 8080;
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/campbellwooddesigns");
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/campbellwooddesigns"
+);
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(session({
-  secret: 'ilovescotchscotchyscotchscotch'
-}));
-app.use(morgan('dev'))
+app.use(
+  session({
+    secret: "ilovescotchscotchyscotchscotch",
+  })
+);
+app.use(morgan("dev"));
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 //  CORS auth
-if (app.get('env') === 'development') {
-  app.use(function(_, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+if (app.get("env") === "development") {
+  app.use(function (_, res, next) {
+    res.header(
+      "Access-Control-Allow-Origin",
+      "https://ancient-lowlands-03560.herokuapp.com/"
+    ); // update to match the domain you will make the request from
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
     next();
   });
 }
@@ -41,7 +51,7 @@ mongoose.connect(
 // // required for passport
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
-require('./config/passport.js')(passport);
+require("./config/passport.js")(passport);
 app.use(routes);
 // Start the API server
 app.listen(PORT, function () {
