d27668215ff926e768d01f466922bcece9a74682
saphirah.pociluyko <sjpcp5@gmail.com>
2020-05-23 09:01:24 -0500

    removed broswer router

--- client/src/index.js
+++ client/src/index.js
@@ -5,11 +5,5 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
